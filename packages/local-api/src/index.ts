c2195019c51e52a31222ebb830e8ffd5547713d8
DevFJD <fduenkel@outlook.com>
2021-02-16 08:23:02 +0100

    enable cli to start local api

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log('Server up');
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log('serving traffic on port:', port);
+  console.log('filename:', filename);
+  console.log('dir:', dir);
 };
