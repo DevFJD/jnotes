75d7526cbbee07f242986ee226449905a1e065a3
DevFJD <fduenkel@outlook.com>
2021-02-16 11:16:12 +0100

    persist via local api

--- packages/local-client/src/state/action-creators/cellsActionCreator.ts
+++ packages/local-client/src/state/action-creators/cellsActionCreator.ts
@@ -1,6 +1,16 @@
 import { ActionType } from '../action-types';
-import { UpdateCellAction, DeleteCellAction, MoveCellAction, InsertCellAfterAction, Direction } from '../actions';
-import { CellTypes } from '../cell';
+import axios from 'axios';
+import { Dispatch } from 'react';
+import {
+  UpdateCellAction,
+  DeleteCellAction,
+  MoveCellAction,
+  InsertCellAfterAction,
+  Direction,
+  Action,
+} from '../actions';
+import { CellTypes, Cell } from '../cell';
+import { RootState } from '../reducers';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -30,5 +40,31 @@ export const insertCellAfter = (id: string | null, cellType: CellTypes): InsertC
       id,
       type: cellType,
     },
+  };
+};
+
+export const fetchCells = () => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({ type: ActionType.FETCH_CELLS });
+    try {
+      const { data }: { data: Cell[] } = await axios.get('/cells');
+      dispatch({ type: ActionType.FETCH_CELLS_COMPLETE, payload: data });
+    } catch (err) {
+      dispatch({ type: ActionType.FETCH_CELLS_ERROR, payload: err.message });
+    }
+  };
+};
+
+export const saveCells = () => {
+  return async (dispatch: Dispatch<Action>, getState: () => RootState) => {
+    const {
+      cells: { data, order },
+    } = getState();
+    const cells = order.map((id) => data[id]);
+    try {
+      axios.post('/cells', { cells });
+    } catch (err) {
+      dispatch({ type: ActionType.SAVE_CELLS_ERROR, payload: err.message });
+    }
   };
 };
--- packages/local-client/src/state/reducers/cellsReducer.ts
+++ packages/local-client/src/state/reducers/cellsReducer.ts
@@ -21,6 +21,24 @@ const initialState: CellsState = {
 
 const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
+    case ActionType.FETCH_CELLS:
+      state.loading = true;
+      state.error = null;
+      return state;
+
+    case ActionType.FETCH_CELLS_COMPLETE:
+      state.order = action.payload.map((cell) => cell.id);
+      state.data = action.payload.reduce((acc, cell) => {
+        acc[cell.id] = cell;
+        return acc;
+      }, {} as CellsState['data']);
+      return state;
+
+    case ActionType.FETCH_CELLS_ERROR:
+      state.loading = false;
+      state.error = action.payload;
+      return state;
+
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
       state.data[id].content = content;
@@ -55,6 +73,10 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       } else {
         state.order.splice(foundIndex + 1, 0, cell.id);
       }
+      return state;
+
+    case ActionType.SAVE_CELLS_ERROR:
+      state.error = action.payload;
       return state;
 
     default:
